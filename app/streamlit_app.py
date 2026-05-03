443f960bbdd28b42c8dc11f8bedffd5ea54d8b42
MaryaD97 <maryamrizwan7@gmail.com>
2026-05-03 15:27:05 +0500

    Refactor HTML structure in streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -727,43 +727,42 @@ if st.session_state.step == 1:
 # STEP 1 — LOAD DATA
 # ==============================
         hero_html = """<div class="hero-shell">
-
         <div class="hero-left">
         
-            <div class="hero-top">
-                <div>
-                    <div class="hero-title">Fraud Decision Engine</div>
-                    <div class="hero-sub">
-                    Decide the lowest-cost action for every transaction — reduce fraud loss while minimizing manual review costs.
-                    </div>
-                </div>
+        <div class="hero-top">
+        <div>
+        <div class="hero-title">Fraud Decision Engine</div>
+        <div class="hero-sub">
+        Decide the lowest-cost action for every transaction — reduce fraud loss while minimizing manual review costs.
+        </div>
+        </div>
         
-                <img class="hero-image" src="https://raw.githubusercontent.com/MaryaD97/customer-risk-intelligence/main/shield_01.jpg"/>
-            </div>
+        <img class="hero-image" src="https://raw.githubusercontent.com/MaryaD97/customer-risk-intelligence/main/shield_01.jpg"/>
+        </div>
         
-            <div class="hero-grid">
+        <div class="hero-grid">
         
-                <div>
-                    <div class="hero-list-title">What You Need</div>
-                    <div class="hero-list">
-                        👤 Customer score or rating<br>
-                        📊 Behavioral signal<br>
-                        💳 Transaction value
-                    </div>
-                </div>
+        <div>
+        <div class="hero-list-title">What You Need</div>
+        <div class="hero-list">
+        👤 Customer score or rating<br>
+        📊 Behavioral signal<br>
+        💳 Transaction value
+        </div>
+        </div>
         
-                <div class="hero-divider"></div>
+        <div class="hero-divider"></div>
         
-                <div>
-                    <div class="hero-list-title">System Output</div>
-                    <div class="hero-list">
-                        🛡️ Detect fraud risk<br>
-                        📉 Estimate financial impact<br>
-                        ✅ Recommend best action
-                    </div>
-                </div>
+        <div>
+        <div class="hero-list-title">System Output</div>
+        <div class="hero-list">
+        🛡️ Detect fraud risk<br>
+        📉 Estimate financial impact<br>
+        ✅ Recommend best action
+        </div>
+        </div>
         
-            </div>
+        </div>
         
         </div>
         </div>"""
